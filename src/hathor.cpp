097469d3c92450929f675260681364bb36e75306
schambers <schambers80@gmail.com>
2022-09-20 22:32:01 -0500

    add chorus effect, get multiple voices working properly, reverb

--- src/hathor.cpp
+++ src/hathor.cpp
@@ -17,18 +17,24 @@ LFO amount knob : A7
 #include "daisysp.h"
 #include "daisy_seed.h"
 
+#define NUM_VOICES 6
+
 using namespace daisysp;
 using namespace daisy;
 
 static MidiUsbHandler midi;
-
 static DaisySeed  hw;
 static MoogLadder filter;
-static Oscillator osc, lfo;
-static Adsr env;
+static Oscillator lfo;
+static Chorus chorus;
 static bool filterEnabled;
 static bool lfoEnabled;
 static ReverbSc DSY_SDRAM_BSS verb;
+
+static Oscillator osc[NUM_VOICES];
+static Adsr env[NUM_VOICES];
+bool gate[NUM_VOICES];
+uint8_t note[NUM_VOICES];
 
 float oscFreq = 440;
 float masterAmp;
@@ -38,7 +44,8 @@ float lfoOutput;
 float filterFreq;
 float verbDry;
 float verbWet;
-bool gate;
+float detuneAmount;
+uint8_t next;
 
 enum AdcChannel {
     masterKnob = 0,
@@ -53,6 +60,8 @@ enum AdcChannel {
     attackKnob,
     drKnob,
     verbAmountKnob,
+    chrFreqKnob,
+    chrAmountKnob,
     NUM_ADC_CHANNELS
 };
 
@@ -60,28 +69,39 @@ static void AudioCallback(AudioHandle::InterleavingInputBuffer  in,
                           AudioHandle::InterleavingOutputBuffer out,
                           size_t                                size)
 {
-    float envOut, oscOutput, output, sigWet;
+    float envOut, output, sigWet;
     for(size_t i = 0; i < size; i += 2)
     {
         // Default to filterFreq in case lfo is not enabled
         float filterMixFreq = filterFreq;
-
         lfoOutput = fmap(lfo.Process() + 0.5, 0, 5000);
 
         // Determine filter frequency, mixing in LFO parameters
-        if (lfoEnabled) {
+        if (lfoEnabled)
+        {
             filterMixFreq = filterFreq * (1 - lfoAmount) + lfoOutput * lfoAmount;
             filter.SetFreq(filterMixFreq);
         }
 
-        envOut = env.Process(gate);
-        osc.SetAmp(envOut);
-        oscOutput = osc.Process();
-        output = filterEnabled ? filter.Process(oscOutput) * 3 : oscOutput;
+        for (uint8_t i = 0; i < NUM_VOICES; i++)
+        {
+            envOut = env[i].Process(gate[i]);
+            osc[i].SetAmp(envOut);
 
+            if (i == 0) {
+                output = osc[0].Process();
+            }
+            else {
+                output = output + osc[i].Process();
+            }
+        }
+
+        output = filterEnabled ? filter.Process(output) * 3 : output;
+
+        output = chorus.Process(output);
         verb.Process( output, 0, &sigWet, 0);
 
-        // left & right combined, mix the oscillators
+        // left & right combined from output and added reverb mix then add amp value for volume
         out[i] = out[i + 1] = (output * verbDry + sigWet * verbWet) * masterAmp;
     }
 }
@@ -119,6 +139,8 @@ int main(void)
     adcConfig[attackKnob].InitSingle(seed::A5);
     adcConfig[drKnob].InitSingle(seed::A2);
     adcConfig[verbAmountKnob].InitSingle(seed::A9);
+    adcConfig[chrFreqKnob].InitSingle(seed::A10);
+    adcConfig[chrAmountKnob].InitSingle(seed::A11);
     hw.adc.Init(adcConfig, NUM_ADC_CHANNELS);
 
     hw.SetAudioBlockSize(4);
@@ -128,13 +150,22 @@ int main(void)
     float shapeValue = 1.0 - hw.adc.GetFloat(shapeKnob);
     uint8_t mappedShape = fmap(shapeValue, 0, 5);
 
+    // Initialize Oscillators & Envelopes
+    for (uint8_t i = 0; i < NUM_VOICES; i++) {
+        osc[i].Init(sample_rate);
+        osc[i].SetWaveform(mappedShape);
+        osc[i].SetAmp(0.0f);
+
+        env[i].Init(sample_rate);
+        env[i].SetTime(ADSR_SEG_ATTACK, getFloat(attackKnob));
+        env[i].SetTime(ADSR_SEG_DECAY, getFloat(drKnob));
+	    env[i].SetTime(ADSR_SEG_RELEASE, getFloat(drKnob));
+        env[i].SetSustainLevel(.25);
+    }
     // Oscillator 1
-    osc.SetWaveform(mappedShape);
-    //osc.SetFreq(oscFreq);
-    //osc.SetAmp(getFloat(masterKnob));
-    
-    osc.Init(sample_rate);
-    osc.SetAmp(0.0f);
+    //osc.SetWaveform(mappedShape);
+    //osc.Init(sample_rate);
+    //osc.SetAmp(0.0f);
 
     // MoogLadder Filter
     filter.Init(sample_rate);
@@ -143,27 +174,24 @@ int main(void)
     // LFO that affects oscillators 1, 2
     lfo.SetWaveform(Oscillator::WAVE_SIN);
     lfo.SetFreq(0.4);
-    //lfo.SetAmp(0.2);
     lfo.SetAmp(0.0f);
     lfo.Init(sample_rate);
-
-    env.Init(sample_rate);
-	env.SetTime(ADSR_SEG_ATTACK, getFloat(attackKnob));
-	env.SetTime(ADSR_SEG_DECAY, getFloat(drKnob));
-	env.SetTime(ADSR_SEG_RELEASE, getFloat(drKnob));
-    env.SetSustainLevel(.25);
 
     verb.Init(sample_rate);
     verb.SetFeedback(0.95f);
     verb.SetLpFreq(16000.0f);
 
+    chorus.Init(sample_rate);
+    chorus.SetLfoFreq(getFloat(chrFreqKnob), .2f);
+    chorus.SetLfoDepth(getFloat(chrAmountKnob), getFloat(chrAmountKnob));
+    chorus.SetDelay(.75f, .9f);
+
     // Initialize the GPIO object
     // Mode: INPUT - because we want to read from the button
     // Pullup: Internal resistor to prevent needing extra components
     GPIO filterSwitch;
-    filterSwitch.Init(seed::D14, GPIO::Mode::INPUT, GPIO::Pull::PULLUP);
-
     GPIO lfoSwitch;
+    filterSwitch.Init(seed::D14, GPIO::Mode::INPUT, GPIO::Pull::PULLUP);
     lfoSwitch.Init(seed::D13, GPIO::Mode::INPUT, GPIO::Pull::PULLUP);
 
     // start callback
@@ -172,15 +200,7 @@ int main(void)
 
     while(1) {
         midi.Listen();
-        //System::Delay(1000);
 
-        // Master knob
-        // midi conversion
-        //float ampValue = getFloat(masterKnob);
-        //osc.SetAmp(0.0f);
-
-        uint8_t mappedShape = fmap(getFloat(shapeKnob), 0, 5);
-        osc.SetWaveform(mappedShape);
 
         filterEnabled = !filterSwitch.Read();
         float filterRes = fmap(getFloat(resKnob), 0, 0.8);
@@ -194,6 +214,20 @@ int main(void)
         lfo.SetFreq(lfoFreq);
         lfoAmount = getFloat(lfoAmountKnob);
 
+        detuneAmount = getFloat(detuneKnob);
+
+        float chrFreq = getFloat(chrFreqKnob);
+        float chrDepth = getFloat(chrAmountKnob);
+        chorus.SetLfoFreq(chrFreq, (chrFreq * 0.9));
+        chorus.SetLfoDepth(chrDepth, chrDepth); 
+
+        uint8_t mappedShape = fmap(getFloat(shapeKnob), 0, 5);
+        for (uint8_t i = 0; i < NUM_VOICES; i++) {
+            osc[i].SetWaveform(mappedShape);
+            env[i].SetTime(ADSR_SEG_ATTACK, getFloat(attackKnob));
+	        env[i].SetTime(ADSR_SEG_RELEASE, getFloat(drKnob));
+        }
+
         while(midi.HasEvents())
         {
             auto msg = midi.PopEvent();
@@ -203,21 +237,27 @@ int main(void)
                 {
                     auto note_msg = msg.AsNoteOn();
                     if(note_msg.velocity != 0) {
-                        osc.SetFreq(mtof(note_msg.note));
-                        gate = true;
+                        next = (next + 1) % NUM_VOICES;
+                        note[next] = note_msg.note;
+                        osc[next].SetFreq(mtof(note_msg.note) );
+                        gate[next] = true;
+                        
                     }
                 }
                 break;
                 case NoteOff:
                 {
-                    gate = false;
+                    auto note_msg = msg.AsNoteOn();
+                    for (uint8_t i = 0; i < NUM_VOICES; i++) {
+                        if (note[i] == note_msg.note) {
+                            note[i] = 0;
+                            gate[i] = false;
+                        }
+                    }
                 }
                 break;
                 default: break;
             }
-
-            env.SetTime(ADSR_SEG_ATTACK, getFloat(attackKnob));
-	        env.SetTime(ADSR_SEG_RELEASE, getFloat(drKnob));
 
             verbWet = (fmap(getFloat(verbAmountKnob), 0, 1));
             verbDry = (-1 + verbWet) * -1;
